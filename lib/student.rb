c0d09f50834c8135d829c8cde9d0550d7dc100d5
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-01 16:59:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,4 +57,14 @@ class Student
   def self.new_from_db(row)
     self.new(row[0], row[1], row[2])
   end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ?
+    LIMIT 1;
+    SQL
+    row = DB[:conn].execute(sql, name)
+    self.new_from_db(row)
+  end
 end
